d004f2b95f40d4f5bf98b48e638a240f4a083976
nasizae <nasizae@gmail.com>
2023-09-27 16:30:36 +0600

    youTube

---
+++ app/src/main/java/com/example/youtube/PlayerListAdapter.kt
@@ -0,0 +1,32 @@
+package com.example.youtube
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView.Adapter
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import com.bumptech.glide.Glide
+import com.example.youtube.databinding.ItemYoutubeListBinding
+
+class PlayerListAdapter(private var playerList:ArrayList<PlayerListModel>):Adapter<PlayerListAdapter.PlayerListHolder>() {
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PlayerListHolder {
+        return PlayerListHolder(ItemYoutubeListBinding.inflate(LayoutInflater.from(parent.context),parent,false))
+    }
+
+    override fun getItemCount()=playerList.size
+
+    override fun onBindViewHolder(holder: PlayerListHolder, position: Int) {
+        holder.bind(playerList.get(position))
+    }
+
+    inner class PlayerListHolder( var binding: ItemYoutubeListBinding):ViewHolder(binding.root) {
+        fun bind(playerListModel: PlayerListModel){
+            with(binding){
+                name.text=playerListModel.name
+                tvCount.text=playerListModel.count.toString()
+                Glide.with(image).load(playerListModel.image).into(image)
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/youtube/PlayerListModel.kt
@@ -0,0 +1,7 @@
+package com.example.youtube
+
+data class PlayerListModel (
+     var image:String?=null,
+    var name:String?=null,
+    var count:Int?=null
+)
